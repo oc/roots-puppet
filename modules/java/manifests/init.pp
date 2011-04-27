6e825b574be099a548d90d72f744cebb5aeb1f2d
Ole Christian Rynning <oc@rynning.no>
2011-04-27 13:21:27 +0200

    WIP: Installing Java (from binary Oracle release)

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -1,28 +1,25 @@
 #class java {
-#  package{
-#    ['jpackage-utils']:
-#    ensure => present,
-#  }
-#  package{
-#    ['java-1.6.0-sun', 'java-1.6.0-sun-devel', 'java-1.6.0-sun-fonts']:
-#    ensure => present,
-#  }
 #
+#  TODO: Install JVM to /usr/java
 #  Exec {
-#    "build_rpms":
-#      command => ""
+#    "install-jvm":
+#      command => "fetch the latest jdk?",
+#      require => [ File['/usr/java/default/bin/java'], File['/usr/java'] ],
 #  }
+#  TODO: Add Symlink /usr/java/default -> latest
+#  TODO: Add Symlink /usr/java/latest  -> jdk
 #
 #  Exec {
 #    "java_exists":
-#      command => "alternatives --set java /usr/lib/jvm/jre-1.6.0-sun/bin/java",
-#      unless  => "alternatives --display java | grep 'points to' | grep -q '/usr/lib/jvm/jre-1.6.0-sun/bin/java'",
-#      require => [ Package['java-1.6.0-sun'], Package['java-1.6.0-sun-devel'], Package['java-1.6.0-sun-fonts'] ],
+#      command => "alternatives --set java /usr/java/default/bin/java",
+#      unless  => "alternatives --display java | grep 'points to' | grep -q '/usr/java/default/bin/java'",
+#      require => [ Exec['install-jvm'] ],
 #  }
 #  Exec {
 #    "javac_exists":
-#      command => 'alternatives --set javac /usr/lib/jvm/java-1.6.0-sun/bin/javac',
-#      unless  => "alternatives --display javac| grep 'points to' | grep -q '/usr/lib/jvm/java-1.6.0-sun/bin/javac'",
-#      require => [ Package['java-1.6.0-sun'], Package['java-1.6.0-sun-devel'], Package['java-1.6.0-sun-fonts'] ],
+#      command => 'alternatives --set javac /usr/java/default/bin/javac',
+#      unless  => "alternatives --display javac| grep 'points to' | grep -q '/usr/java/default/bin/javac'",
+#      require => [ ],
 #  }
+#  TODO: Add JAVA_HOME to /etc/profile
 #}
