c2af30e245cbd7a26b6770125ec729299c8f0f4f
Ole Christian Rynning <oc@rynning.no>
2011-04-28 13:58:44 +0200

    Should be class

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,22 +1,24 @@
 # implicit param $name
-user { 'oc':
-  ensure  => present,
-  uid     => 1337,
-  gid     => '10',
-  comment => 'Ole Christian Rynning',
-  shell   => "/bin/bash",
-  home    => "/home/oc",
-  managehome => true,
-}
+class users {
+  user { 'oc':
+    ensure  => present,
+    uid     => 1337,
+    gid     => '10',
+    comment => 'Ole Christian Rynning',
+    shell   => "/bin/bash",
+    home    => "/home/oc",
+    managehome => true,
+  }
 
-group { 'bekk': ensure => present, gid => 2000 }
+  group { 'bekk': ensure => present, gid => 2000 }
 
-user  { 'bekk':
-  ensure => present,
-  uid => 2000,
-  gid => 2000,
-  comment => 'BEKK Deployment User',
-  shell   => "/bin/bash",
-  home    => "/u01/bring",
-  managehome => true,
+  user  { 'bekk':
+    ensure => present,
+    uid => 2000,
+    gid => 2000,
+    comment => 'BEKK Deployment User',
+    shell   => "/bin/bash",
+    home    => "/u01/bring",
+    managehome => true,
+  }
 }
