c1f959a6fa8da82a783fa38caca8dd555c4f70d1
Ole Christian Rynning <oc@rynning.no>
2011-04-28 13:57:37 +0200

    Explicit users

--- manifests/users.pp
+++ manifests/users.pp
@@ -1,31 +1,22 @@
 # implicit param $name
-define create_user($group => "$name", $userhome => "/home/$name", $shell => "/bin/bash", $comment => "$fullname") {
-  user { $name:
-    ensure  => present,
-    comment => "$comment",
-    shell   => "/bin/bash",
-    home    => "$userhome"
-  }
-
-  group { $group:
-    ensure => present,
-  }
-
-  exec { "$name userhome":
-    command => "/bin/cp -R /etc/skel $userhome; /bin/chown -R $name:$name $userhome",
-    creates => "$userhome",
-    require => User[$name],
-  }
-
-  file { "$userhome":
-    ensure  => directory,
-    owner   => "$name",
-    group   => "$group",
-    mode    => 700,
-    require => Exec["$name userhome"]
-  }
+user { 'oc':
+  ensure  => present,
+  uid     => 1337,
+  gid     => '10',
+  comment => 'Ole Christian Rynning',
+  shell   => "/bin/bash",
+  home    => "/home/oc",
+  managehome => true,
 }
 
-create_user { 'bekk': userhome => '/u01/bring' }
-create_user { 'oc':   group => 'staff' }
-create_user { 'steininge': group => 'staff' }
+group { 'bekk': ensure => present, gid => 2000 }
+
+user  { 'bekk':
+  ensure => present,
+  uid => 2000,
+  gid => 2000,
+  comment => 'BEKK Deployment User',
+  shell   => "/bin/bash",
+  home    => "/u01/bring",
+  managehome => true,
+}
