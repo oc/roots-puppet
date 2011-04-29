de5a25670ebe585d9a018f5b4b7a85b72c277754
Ole Christian Rynning <oc@rynning.no>
2011-04-29 13:06:34 +0200

    Styled java-module (wip)

--- modules/java/manifests/init.pp
+++ modules/java/manifests/init.pp
@@ -1,25 +1,23 @@
 #class java {
 #
 #  TODO: Install JVM to /usr/java
-#  Exec {
-#    "install-jvm":
+#  exec { "Install Oracle JDK":
 #      command => "fetch the latest jdk?",
 #      require => [ File['/usr/java/default/bin/java'], File['/usr/java'] ],
 #  }
 #  TODO: Add Symlink /usr/java/default -> latest
 #  TODO: Add Symlink /usr/java/latest  -> jdk
 #
-#  Exec {
-#    "java_exists":
+#  exec { "set Oracle JDK as default java alternative":
 #      command => "alternatives --set java /usr/java/default/bin/java",
 #      unless  => "alternatives --display java | grep 'points to' | grep -q '/usr/java/default/bin/java'",
-#      require => [ Exec['install-jvm'] ],
+#      require => [ Exec['Install Oracle JDK'] ],
 #  }
-#  Exec {
-#    "javac_exists":
+#
+#  exec { "set Oracle JDK as default javac alternative":
 #      command => 'alternatives --set javac /usr/java/default/bin/javac',
 #      unless  => "alternatives --display javac| grep 'points to' | grep -q '/usr/java/default/bin/javac'",
 #      require => [ ],
 #  }
-#  TODO: Add JAVA_HOME to /etc/profile
+#  TODO: Append JAVA_HOME to /etc/profile
 #}
